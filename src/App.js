006fae62e9bbc30880c1b69e2cfd9e6bc935fd44
Anne H F <annehjefro@gmail.com>
2018-11-07 17:11:34 +0100

    byggmester bob

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Edit <code>src/App.js</code> and save to reload.	
+		    Kan det fikses?<\br>
+                    Klart det kan.<\br>
+              Edit <code>src/App.js</code> and save to reload.	
           </p>
           <a
             className="App-link"
