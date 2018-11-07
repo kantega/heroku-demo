9d714a490eb8c07a496319d18cd123419ed7aeb0
h182032 <h182032@stud.hib.no>
2018-11-07 17:11:45 +0100

    Litta app

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		   Ei litta app	
+		   Ei litta lure app	
           </p>
           <a
             className="App-link"
