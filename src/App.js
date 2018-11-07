2a4394dcb0ef7b0048b26529d0712fa6c3a710f1
Simen Gjerde <Simen_Gjerde@158-37-195-239.hvl.no>
2018-11-07 16:49:34 +0100

    Simen was here

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
           <p>
 		    Edit <code>src/App.js</code> and save to reload.	
           </p>
+	  <p>
+		    En enorm endring
+	  </p>
           <a
             className="App-link"
             href="https://reactjs.org"
