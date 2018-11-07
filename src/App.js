4682eabc62040c1de59835910828149118b9f448
OysteinKnudsen <oysteinknudsen96@gmail.com>
2018-11-07 17:03:37 +0100

    pull request for changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    AUTOMATIC DEPLOYMENT	
+		    pull request??? 	
           </p>
           <a
             className="App-link"
