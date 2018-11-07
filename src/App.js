f7252f91a5a8e6374822bee8f707563af2e83010
OysteinKnudsen <oysteinknudsen96@gmail.com>
2018-11-07 16:56:38 +0100

    Automatic deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    ADRIAN E SLOW	
+		    AUTOMATIC DEPLOYMENT	
           </p>
           <a
             className="App-link"
