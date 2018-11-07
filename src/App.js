f6c6ba8f46f64e90ddbd69a821bfffe0a732f2d6
Arne <arnekva@gmail.com>
2018-11-07 16:37:50 +0100

    Arne test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Edit <code>src/App.js</code> and save to reload.	
+		   Endring er gjort. Edit <code>src/App.js</code> and save to reload.	
           </p>
           <a
             className="App-link"
@@ -26,3 +26,4 @@ class App extends Component {
 }
 
 export default App;
+
