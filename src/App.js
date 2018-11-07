779e4d1f56197688f2d7401c00d0d4052ec89e28
Helene <helene-nordal@hotmail.com>
2018-11-07 17:02:14 +0100

    I made som changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            You did it again
+            You did it now
           </a>
         </header>
       </div>
