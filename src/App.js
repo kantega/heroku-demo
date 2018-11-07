a7fb9a8c82dabd13f4b45eef33d69d4bb09b2dfa
OysteinKnudsen <oysteinknudsen96@gmail.com>
2018-11-07 16:38:15 +0100

    Adrian e slow

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Edit <code>src/App.js</code> SNAKKES	
+		    ADRIAN E SLOW	
           </p>
           <a
             className="App-link"
