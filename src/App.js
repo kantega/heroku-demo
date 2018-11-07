5d1fa31823564bb2ece79f146287030ec4cfc844
Adrian <addi180203@gmail.com>
2018-11-07 16:58:17 +0100

    yo 1.0

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            øystein steintung 
+            bønna bønna 
           </a>
         </header>
       </div>
