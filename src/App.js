b58ca484c6d72dc28bec68ceacabefe5f4221d04
145168 <h145168@stud.hib.no>
2018-11-07 16:51:26 +0100

    fredrik was here

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <h1>
 		    Edit <code>src/App.js</code> and save to reload.	
-          </p>
+          </h1>
           <a
             className="App-link"
             href="https://reactjs.org"
