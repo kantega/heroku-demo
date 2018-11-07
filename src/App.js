5ac4a374d0b00982a44765fb6d4e0623cf1044d5
145168 <h145168@stud.hib.no>
2018-11-07 17:11:24 +0100

    endret ting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>
+        <p>
+	oioioi!!
+	</p>  
+	<h1>
 		    Edit <code>src/App.js</code> and save to reload.	
           </h1>
           <a
