823b8cb2d229831b4fa1a76a5236e6cc1db28837
Arne <arneflatekval@hotmail.com>
2018-11-07 17:00:38 +0100

    First commit in branch

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ class App extends Component {
 		    Edit <code>src/App.js</code> and save to reload. </br> 
 		    Change made!!	
           </p>
+	<p>
+		    NEW BRANCH
+	</p>
           <a
             className="App-link"
             href="https://reactjs.org"
