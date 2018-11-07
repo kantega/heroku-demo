61ad948e18e9883f90e8e2752a5e07817417a0d8
Arne <arnekva@gmail.com>
2018-11-07 16:43:54 +0100

    Test 2

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		   Endring er gjort. Edit <code>src/App.js</code> and save to reload.	
-          </p>
+		   Jeg har gjort endringer! Det er gjort. Edit <code>src/App.js</code> and save to reload.	</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -25,5 +24,8 @@ class App extends Component {
   }
 }
 
-export default App;
+xport default App;:x
+:x
+:d
+:q
 
