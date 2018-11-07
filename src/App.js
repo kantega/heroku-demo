34a18372f0e7c36ed1976f4766cfd20389029005
181185 <jacobaas@hotmail.com>
2018-11-07 17:45:18 +0100

    added eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; // eslint-line-disabled
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,18 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-		    Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <button>Test</button>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            HELLO WORLD
-            Test
-            dasdasdsadsdasdasdas
           </a>
         </header>
       </div>
