71415ebdb8301d28b4bae135683e0c02ea14999a
Gøran Basso <goran.basso@kantega.no>
2018-11-14 21:02:23 +0100

    goran was here

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Edit <code>src/App.js</code> and save to reload.	
+		    Her har vi nå gjort endringer på den lille appen vår	
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            reactjs.org
           </a>
         </header>
       </div>
