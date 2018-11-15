4ffc6988f6f395124559c960f9a05dbc3411fad2
Adrian Tysnes <adrian@tysn.es>
2018-11-15 17:43:32 +0100

    endring 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Hei verden 	
+		    Hei verden, endring nr 2.	
           </p>
           <a
             className="App-link"
