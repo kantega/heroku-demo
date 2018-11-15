5639fd21ca449f287608552a8400179b35a75919
Adrian Tysnes <adrian@tysn.es>
2018-11-15 17:28:17 +0100

    hello world

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Edit <code>src/App.js</code> and save to reload.	
+		    Hei verden 	
           </p>
           <a
             className="App-link"
